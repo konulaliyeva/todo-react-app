9d9b383baa62f067dcb8930447d3f5503427ed22
Konul Aliyeva <konulaliyeva@Konuls-MacBook-Pro.local>
2022-07-22 15:27:06 +0400

    Fix bug on edit section

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react';
 import './index.css';
 import ListItemsForm from './components/ListItemsForm';
 import InputForm from './components/InputForm';
+
 const dummyListItems = [
   {
     id: 'e1',
     title: 'Do homework',
-    date: new Date(),
+    date: new Date()
   },
   {
     id: 'e2',
     title: 'Wash dishes',
     date: new Date()
-
   }
 ];
 
@@ -21,17 +21,13 @@ function App() {
   const [items, setItems] = useState(dummyListItems);
   const [itemId, setItemId] = useState(null);
 
-
-  
   const handleInputValue = (event) => {
     setInputValue(event.target.value);
   };
 
-
- 
   const handleSubmitForm = (event) => {
     event.preventDefault();
-    
+
     if (itemId) {
       let index = items.findIndex((item) => item.id === itemId);
       items[index].title = inputValue;
@@ -41,27 +37,27 @@ function App() {
         id: Math.random(),
         date: new Date()
       };
-      
+      // if(inputValue ===""){
+      //   setItems('');
+      //   document.querySelector('.form__input').classList.add('border');
+
+      // }
       setItems((prevItems) => {
         return [listItem, ...prevItems];
       });
-    
+    }
     setInputValue('');
   };
-   
-  }
+
   const handleDeleteButton = (id) => {
-    
     setItems(items.filter((item) => item.id !== id));
   };
 
   const handleEditButton = ({ title, id }) => {
     setItemId(id);
     setInputValue(title);
-   
   };
-  
-  
+
   return (
     <div className="container">
       <h1>Tu .. du ..</h1>
@@ -72,6 +68,7 @@ function App() {
       />
       <ListItemsForm
         items={items}
+        inputValue={inputValue}
         handleDeleteButton={handleDeleteButton}
         handleEditButton={handleEditButton}
       />
@@ -80,13 +77,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-
-  
-
-
